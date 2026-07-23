cc31f79de17ec0d56ab274ca61c41be2804e2851
Tori1011vy <victorialangguth@gmail.com>
2026-07-23 10:54:05 -0500

    Enhance variable definitions in variables.tf
    Added descriptions for existing variables and created new variables for root volume size and log retention days.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,40 @@
 variable "aws_region" {
-  type    = string
-  default = "us-east-1"
+  description = "AWS region for all resources"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "project_name" {
-  type    = string
-  default = "containerwatch"
+  description = "Name prefix applied to every resource"
+  type        = string
+  default     = "containerwatch"
 }
 
 variable "instance_type" {
-  type    = string
-  default = "t3.small"
+  description = "EC2 instance type (t3.small: 2 vCPU / 2 GiB, sized for six containers)"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "root_volume_size" {
+  description = "Root EBS volume size in GiB (Prometheus TSDB plus Docker images)"
+  type        = number
+  default     = 30
+}
+
+variable "log_retention_days" {
+  description = "CloudWatch Logs retention period"
+  type        = number
+  default     = 14
 }
 
 variable "admin_cidr" {
-  description = "Your public IP in CIDR format, for example 203.0.113.10/32"
+  description = "Administrator public IP in CIDR form, for example 203.0.113.10/32"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of an existing EC2 key pair, used for SSH and for CI/CD deployment"
   type        = string
 }
 
